e4e2e9d22ec6d4375fb0b0e41db5e759df0d2991
carlosmarte23 <carlosmarte23@gmail.com>
2025-11-08 13:45:18 -0500

    feat: add clickable applied state with green style to the apply button in job-listings

---
+++ 01-javascript/js/search-filters.js
@@ -0,0 +1,10 @@
+const jobsListingsSection = document.querySelector(".job-listings");
+
+jobsListingsSection.addEventListener("click", (event) => {
+  const element = event.target;
+
+  if (element.classList.contains("btn-apply")) {
+    element.textContent = "¡Aplicado!";
+    element.classList.add("applied");
+  }
+});
