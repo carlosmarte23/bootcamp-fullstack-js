4e25ba4b87f6de7107162313a52c55c765b65c9e
carlosmarte23 <carlosmarte23@gmail.com>
2025-11-08 14:53:47 -0500

    feat: added job filter  for the technology filter select.

--- 01-javascript/js/search-filters.js
+++ 01-javascript/js/search-filters.js
@@ -1,3 +1,7 @@
+// search-filters.js
+
+// Button Apply State Change
+
 const jobsListingsSection = document.querySelector(".job-listings");
 
 jobsListingsSection.addEventListener("click", (event) => {
@@ -7,4 +11,32 @@ jobsListingsSection.addEventListener("click", (event) => {
     element.textContent = "¡Aplicado!";
     element.classList.add("applied");
   }
+});
+
+// Filter Jobs by Select Inputs
+
+const filterTechnology = document.getElementById("filter-technology");
+// const filterLocation = document.getElementById("filter-location");
+// const filterContractyType = document.getElementById("filter-contract-type");
+// const filterExperienceLevel = document.getElementById("filter-experience");
+
+filterTechnology.addEventListener("change", () => {
+  const selectedTechnology = filterTechnology.value.toLowerCase();
+  const jobListings = document.querySelectorAll(".job-listing");
+
+  jobListings.forEach((job) => {
+    const jobText = job.textContent.toLowerCase();
+
+    //if no filter is selected, show all jobs
+    if (selectedTechnology === "") {
+      job.style.display = "";
+      return;
+    }
+
+    if (jobText.includes(selectedTechnology)) {
+      job.style.display = "";
+    } else {
+      job.style.display = "none";
+    }
+  });
 });
