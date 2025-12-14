cde649a74a1f5d4d7fae3e0d4c7522bf81402298
carlosmarte23 <carlosmarte23@gmail.com>
2025-12-14 11:07:58 -0500

    feat: Enhance Link component with active state styling and improved color scheme

--- 02-react/src/components/Link.jsx
+++ 02-react/src/components/Link.jsx
@@ -2,6 +2,14 @@ import { useRouter } from "../hooks/useRouter";
 import styles from "./Link.module.css";
 export function Link({ href, children, ...props }) {
   const { navigateTo } = useRouter();
+
+  const currentPath = window.location.pathname;
+  console.log(
+    `Current path: ${currentPath}, matched href: ${href}, ${
+      currentPath === href
+    }`
+  );
+
   const handleClick = (event) => {
     event.preventDefault();
 
@@ -12,7 +20,12 @@ export function Link({ href, children, ...props }) {
   };
 
   return (
-    <a href={href} onClick={handleClick} className={styles.link} {...props}>
+    <a
+      href={href}
+      onClick={handleClick}
+      className={`styles.link ${currentPath === href ? styles.active : ""}`}
+      {...props}
+    >
       {children}
     </a>
   );
