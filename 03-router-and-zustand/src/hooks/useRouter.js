0141cadc23a473171d03c9bb9de4edf9a5cdbc2c
carlosmarte23 <carlosmarte23@gmail.com>
2025-12-25 18:11:02 -0500

    fix: return the useRouter property currentPath but using react-router implementation of useLocation hook

--- 03-router-and-zustand/src/hooks/useRouter.js
+++ 03-router-and-zustand/src/hooks/useRouter.js
@@ -1,11 +1,14 @@
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 export function useRouter() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   function navigateTo(path) {
     navigate(path);
   }
 
-  return { navigateTo };
+  const currentPath = location.pathname;
+
+  return { currentPath, navigateTo };
 }
