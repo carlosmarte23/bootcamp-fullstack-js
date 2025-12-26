ed86dca240b6937499471cd82f1a7e1b1818f7e9
carlosmarte23 <carlosmarte23@gmail.com>
2025-12-25 19:05:54 -0500

    feat: add styles to JobDetail component to use CSS modules.

--- 03-router-and-zustand/src/pages/JobDetail.jsx
+++ 03-router-and-zustand/src/pages/JobDetail.jsx
@@ -1,43 +1,44 @@
+import styles from "./JobDetail.module.css";
+
 export function JobDetail() {
   return (
-    <main class="job-details-page">
-      <nav class="job-breadcrumb">
+    <main className={styles.jobDetailsPage}>
+      <nav className={styles.jobBreadcrumb}>
         <a href="jobs.html">Empleos</a>
         <span>/</span>
-        <a href="#">Ingeniero de Software Senior</a>
+        <a href="#" className={styles.currentJob}>
+          Ingeniero de Software Senior
+        </a>
       </nav>
-      <div class="job-details">
-        <header class="job-header">
-          <div class="job-meta">
+      <div className={styles.jobDetails}>
+        <header className={styles.jobHeader}>
+          <div>
             <h1>Ingeniero de Software Senior</h1>
             <p>
-              <span class="job-company">Tech Solutions Inc.</span> -
-              <span class="job-location">Remoto</span>
+              <span>Tech Solutions Inc.</span> -<span>Remoto</span>
             </p>
           </div>
-          <a href="#" class="button btn-apply">
+          <a href="#" className={`button`}>
             Aplicar ahora
           </a>
         </header>
 
-        <section class="job-section">
-          <div class="job-description">
-            <h2>Descripción del puesto</h2>
-            <p>
-              Tech Solutions Inc. está buscando un Ingeniero de Software Senior
-              altamente motivado y experimentado para unirse a nuestro equipo
-              remoto. El candidato ideal tendrá una sólida formación en
-              desarrollo de software, con experiencia en el diseño, desarrollo e
-              implementación de soluciones de software escalables y de alto
-              rendimiento. Como Ingeniero de Software Senior, usted será
-              responsable de liderar proyectos de desarrollo, mentorizar a
-              ingenieros junior y colaborar con equipos multifuncionales para
-              entregar productos de software de alta calidad.
-            </p>
-          </div>
+        <section className={styles.jobSection}>
+          <h2>Descripción del puesto</h2>
+          <p>
+            Tech Solutions Inc. está buscando un Ingeniero de Software Senior
+            altamente motivado y experimentado para unirse a nuestro equipo
+            remoto. El candidato ideal tendrá una sólida formación en desarrollo
+            de software, con experiencia en el diseño, desarrollo e
+            implementación de soluciones de software escalables y de alto
+            rendimiento. Como Ingeniero de Software Senior, usted será
+            responsable de liderar proyectos de desarrollo, mentorizar a
+            ingenieros junior y colaborar con equipos multifuncionales para
+            entregar productos de software de alta calidad.
+          </p>
         </section>
 
-        <section class="job-section">
+        <section className={styles.jobSection}>
           <h2>Responsabilidades</h2>
           <ul>
             <li>
@@ -56,7 +57,7 @@ export function JobDetail() {
           </ul>
         </section>
 
-        <section class="job-section">
+        <section className={styles.jobSection}>
           <h2>Requisitos</h2>
           <ul>
             <li>Licenciatura en Informática o campo relacionado.</li>
@@ -72,7 +73,7 @@ export function JobDetail() {
           </ul>
         </section>
 
-        <section class="job-section">
+        <section className={styles.jobSection}>
           <h2>Acerca de la empresa</h2>
           <p>
             Tech Solutions Inc. es una empresa de tecnología innovadora que se
@@ -84,8 +85,8 @@ export function JobDetail() {
             profesional continuo.
           </p>
         </section>
-        <footer class="job-apply-footer">
-          <a href="#" class="button btn-apply">
+        <footer className={styles.jobApplyFooter}>
+          <a href="#" className={`button`}>
             Aplicar ahora
           </a>
         </footer>
