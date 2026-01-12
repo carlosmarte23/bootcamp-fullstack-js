6d96fcb05992f844377e2b77fc3655cbd7b28782
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-12 15:37:56 -0500

    style(header): refactor profile link markup and add favorites badge styles

--- 03-router-and-zustand/src/components/Header.jsx
+++ 03-router-and-zustand/src/components/Header.jsx
@@ -33,19 +33,23 @@ export function Header() {
         <Link href="/search">Empleos</Link>
         <Link href="/contact">Contacto</Link>
         {isLoggedIn ? (
-          <Link href="/profile">
-            Profile{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="#aa0303"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
-            </svg>{" "}
-            {favoritesCount()}
+          <Link href="/profile" className={styles.favoriteLink}>
+            <span className={styles.favoriteLabel}>Profile</span>
+            <span className={styles.favoriteBadge}>
+              <svg
+                className={styles.heartIcon}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
+              </svg>
+              <span className={styles.favoriteCount}>{favoritesCount()}</span>
+            </span>
           </Link>
         ) : (
           ""
