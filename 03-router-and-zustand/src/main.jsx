9ef63f9d97c5874138d6820cde2a6c72488d6c98
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-12 13:59:43 -0500

    refactor: remove AuthProvider from main.jsx since we use global state with zustand

--- 03-router-and-zustand/src/main.jsx
+++ 03-router-and-zustand/src/main.jsx
@@ -1,14 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { AuthProvider } from "./context/authContext.jsx";
 
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </BrowserRouter>
 );
