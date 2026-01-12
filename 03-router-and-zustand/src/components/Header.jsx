9f4982f2e80d7712615efbec66e4270ff55003d5
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-12 15:30:29 -0500

    feat(header): add profile link with favorites badge for logged-in users

--- 03-router-and-zustand/src/components/Header.jsx
+++ 03-router-and-zustand/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import { useAuthStore } from "../store/authStore.js";
+import { useFavoritesStore } from "../store/favoritesStore.js";
 
 import styles from "./Header.module.css";
 import { Link } from "./Link.jsx";
 
 export function Header() {
+  const { favoritesCount } = useFavoritesStore();
+  const { isLoggedIn } = useAuthStore();
+
   return (
     <header>
       <h1 className={styles.logo}>
@@ -28,6 +32,24 @@ export function Header() {
         <Link href="/">Inicio</Link>
         <Link href="/search">Empleos</Link>
         <Link href="/contact">Contacto</Link>
+        {isLoggedIn ? (
+          <Link href="/profile">
+            Profile{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="#aa0303"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
+            </svg>{" "}
+            {favoritesCount()}
+          </Link>
+        ) : (
+          ""
+        )}
       </nav>
 
       <div className={styles.actions}>
