c7bbdd4f29d863c612c056637bdbec533c8af461
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-25 17:02:09 -0400

    feat: add health endpoint

--- 04-express/index.js
+++ 04-express/index.js
@@ -8,6 +8,13 @@ app.get("/", (req, res) => {
   res.send("Hola desde express!");
 });
 
+app.get("/health", (req, res) => {
+  return res.json({
+    status: "ok",
+    uptime: process.uptime(),
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor express levantado en http://localhost:${PORT}`);
 });
