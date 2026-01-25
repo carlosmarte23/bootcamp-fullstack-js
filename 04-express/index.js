5987188ad0285e906c7e578ca73676a93b6b2e6b
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-25 16:53:10 -0400

    feat: add simple express server

---
+++ 04-express/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = process.env.PORT || 1234;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola desde express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor express levantado en http://localhost:${PORT}`);
+});
