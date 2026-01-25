705a7dd4f0c8a42f1e74dd2e49a6c4ea93877c87
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-25 13:00:34 -0400

    feat: support creating users via POST /users with UUID ids

--- 04-node/server.js
+++ 04-node/server.js
@@ -1,5 +1,7 @@
+import { randomUUID } from "node:crypto";
 import http from "node:http";
 import { uptime } from "node:process";
+import { json } from "node:stream/consumers";
 
 process.loadEnvFile();
 const port = process.env.PORT ?? 3000;
@@ -10,23 +12,55 @@ function sendJson(res, status, data) {
   return res.end(JSON.stringify(data));
 }
 
-const server = http.createServer((req, res) => {
-  console.log(`Petición recibida:`, req.method, req.url);
+let users = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Midu",
+  },
+];
 
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  if (req.url === "/") {
-    return res.end("Hola desde tu primer servidor de Node.js 🔥 👍🏾");
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  console.log(`Petición recibida:`, method, url);
+
+  if (method === "GET") {
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    if (url === "/") {
+      return res.end("Hola desde tu primer servidor de Node.js 🔥 👍🏾");
+    }
+
+    if (url === "/users") {
+      return sendJson(res, 200, { users });
+    }
+
+    if (req.url === "/health") {
+      return sendJson(res, 200, { status: "ok", uptime: uptime() });
+    }
+
+    return sendJson(res, 404, { message: "Not found" });
+  } else if (method === "POST") {
+    if (req.url === "/users") {
+      const body = await json(req);
+
+      if (!body || !body.name) {
+        return sendJson(res, 400, { message: "Name is required" });
+      }
+
+      const newUser = {
+        id: randomUUID(),
+        name: body.name,
+      };
+      users.push(newUser);
+      return sendJson(res, 201, { message: "User created" });
+    }
+  } else {
+    return sendJson(res, 405, { message: "Method not allowed" });
   }
-
-  if (req.url === "/users") {
-    return sendJson(res, 200, { name: "John", lastName: "Doe", age: 30 });
-  }
-
-  if (req.url === "/health") {
-    return sendJson(res, 200, { status: "ok", uptime: uptime() });
-  }
-
-  return sendJson(res, 404, { message: "Not found" });
 });
 
 server.listen(port, () => {
