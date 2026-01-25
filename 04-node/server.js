ec28568b3a0bc497ca1aa92c3344e5b10a50021c
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-25 11:59:32 -0400

    feat: add basic routes and 404 handling to Node server

--- 04-node/server.js
+++ 04-node/server.js
@@ -5,8 +5,19 @@ const port = process.env.PORT ?? 3000;
 
 const server = http.createServer((req, res) => {
   console.log(`Petición recibida:`, req.method, req.url);
+
   res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.end("Hola desde tu primer servidor de Node.js 🔥 👍🏾");
+  if (req.url === "/") {
+    return res.end("Hola desde tu primer servidor de Node.js 🔥 👍🏾");
+  }
+
+  if (req.url === "/users") {
+    res.setHeader("Content-Type", "application/json");
+    return res.end(JSON.stringify({ name: "John", lastName: "Doe", age: 30 }));
+  }
+
+  res.statusCode = 404;
+  return res.end("404 Not Found");
 });
 
 server.listen(port, () => {
