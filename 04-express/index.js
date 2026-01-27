103b14ed7486fe95c5bb7038f993b4b42abbc8b7
carlosmarte23 <carlosmarte23@gmail.com>
2026-01-27 16:57:13 -0400

    feat: add jobs endpoint returning in memory object data

--- 04-express/index.js
+++ 04-express/index.js
@@ -4,6 +4,12 @@ process.loadEnvFile();
 const PORT = process.env.PORT || 1234;
 const app = express();
 
+const jobs = [
+  { id: 1, title: "Frontend Developer", location: "Remote" },
+  { id: 2, title: "Backend Developer", location: "Barcelona" },
+  { id: 3, title: "Fullstack Developer", location: "Remote" },
+];
+
 app.use((req, res, next) => {
   console.log("--- Nueva petición recibida ---");
   console.log("Fecha:", new Date().toLocaleString());
@@ -14,6 +20,10 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.send("Hola desde express!");
+});
+
+app.get("/jobs", (req, res) => {
+  res.json(jobs);
 });
 
 app.get("/health", (req, res) => {
